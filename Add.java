fa1304a7c675962afd70e947e699885e6c628fa0
권형택 <naht94@hanmail.net>
2023-03-10 18:01:44 +0900

    add 수정

--- Add.java
+++ Add.java
@@ -1,13 +1,16 @@
 public class Add {
     int a;
+    int sol;
     int b;
     public Add(int a, int b){
         this.a = a;
         this.b = b;
+        sol = this.a+this.b;
     }
     public int plus(){
-        return this.a+this.b;
+        return sol;
     }
+
 
     public static void main(String[] args) {
         Add number = new Add(2,3);
