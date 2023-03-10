e34201ed8f6d0b92166a4d9b399324efdcf35532
유승연 <seong1542@naver.com>
2023-03-10 16:45:00 +0900

    modified

--- Add.java
+++ Add.java
@@ -5,7 +5,13 @@ public class Add {
         this.a = a;
         this.b = b;
     }
-    public int add(Add number){
-        return number.a+number.b;
+    public int plus(){
+        return this.a+this.b;
+    }
+
+    public static void main(String[] args) {
+        Add number = new Add(2,3);
+        int result = number.plus();
+        System.out.println(result);
     }
 }
