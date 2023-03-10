810a8b8ec9c31c48721be7b224090de053361217
koesnam <76582376+10kseok@users.noreply.github.com>
2023-03-10 17:38:06 +0900

    Add addable number
    기존에 2개만 더할 수 있는 Add를 3개를 더하는 구조로 바꿧습니다.

--- Add.java
+++ Add.java
@@ -1,16 +1,19 @@
-public class Add {
+public class AddOnlyInt3 {
     int a;
     int b;
-    public Add(int a, int b){
+    
+    public Add(int a, int b, int c) {
         this.a = a;
         this.b = b;
+        this.c = c;
     }
-    public int plus(){
-        return this.a+this.b;
+
+    public int plus() { 
+        return this.a + this.b + this.c;
     }
 
     public static void main(String[] args) {
-        Add number = new Add(2,3);
+        Add number = new Add(7,15);
         int result = number.plus();
         System.out.println(result);
     }
